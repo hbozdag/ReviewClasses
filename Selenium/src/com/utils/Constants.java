b2c981e5b089659fe78bae6d0e2d87f8980868de
harunbozdag <harunbozdag@gmail.com>
2020-01-08 18:26:52 -0600

    Jan 7

--- Selenium/src/com/utils/Constants.java
+++ Selenium/src/com/utils/Constants.java
@@ -7,4 +7,9 @@ public class Constants {
 
 	public static final String SytaxUrl="http://jiravm.centralus.cloudapp.azure.com:8081/index.html";
 
+	public static final String SYNTAX_PRACTICE_URL="http://jiravm.centralus.cloudapp.azure.com:8081/index.html";
+
+	public static final String TesterSiteUrl="http://secure.smartbearsoftware.com/samples/testcomplete11/WebOrders/login.aspx";
+	
+
 }
--- Selenium/src/com/zreview01/Constants.java
+++ Selenium/src/com/zreview01/Constants.java
@@ -6,4 +6,7 @@ public class Constants {
 	
 	public static final String SytaxUrl="http://jiravm.centralus.cloudapp.azure.com:8081/index.html";
 
+	
+	public static final String ToolsQA= "https://www.toolsqa.com/automation-practice-form/";
+	
 }
